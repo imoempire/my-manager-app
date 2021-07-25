ee233bb64601982d7e6db89d4f6663f5f447bdb0
imoempire <mrimoempire@gmail.com>
2021-07-25 08:35:55 +0000

    passing props

--- src/Components/EmpolyeeList.jsx
+++ src/Components/EmpolyeeList.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Employee from '../Components/Employee';
 
-export default function EmpolyeeList({Employees}) {
+export default function EmpolyeeList({Employees, handleDelete, handleEdit}) {
     const Employ = Employees.map((employ)=>{
         return (
             <Employee 
              Employ={employ}
+             handleDelete={handleDelete}
+             handleEdit={handleEdit}
              />
         );
 
